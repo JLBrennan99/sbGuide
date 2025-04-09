2acf7936bf27ca8d5b1cf88a3adc745adc445481
yori <james946@comcast.net>
2025-04-08 21:07:43 -0400

    deck importing

---
+++ main.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+class Deck:
+    min_size = 60
+
+    def __init__(self, name: str, main: dict, side: dict):
+        self.name = name
+        self.main = main
+        self.side = side
+
+    @classmethod
+    def from_list(cls, name: str, decklist: str):
+        mb, sb = decklist.split("\n\n")
+        
+        main = defaultdict(int)
+        for line in mb.splitlines():
+            q, n = line.split(sep=" ", maxsplit=1)
+            main[n] += int(q)
+
+        # side = { n: q for q, n in [line.split(" ") for line in sb.splitlines()] }
+        # sb_lines = [line.split(sep=" ", maxsplit=1) for line in sb.splitlines()]
+        # side = {n: q for q, n in sb_lines}
+
+        side = defaultdict(int)
+        for line in sb.splitlines():
+            q, n = line.split(sep=" ", maxsplit=1)
+            side[n] += int(q)
+
+        return cls(name, main, side)
+
+    # hashmap mainDeck;
+    # hashmap sideDeck;
+    # string name;
+
+    # public Deck(hashmap main, hashmap side, string n){
+    #     this.mainDeck = main;
+    #     this.sideDeck = side;
+    #     this.name = n;  
+    # Deck d = new Deck({},{},"titan");
+
+decklist = """1 Swamp
+2 Swamp
+
+1 Mountain
+2 Mountain
+"""
+
+
+deck_1 = Deck("energy", {}, {})
+deck_2 = Deck.from_list("titan", decklist)
+
+print(deck_2.name)
+print(deck_2.main)
+print(deck_2.side)
+
+print(Deck.min_size)
