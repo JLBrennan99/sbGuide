52c462e66fcc013b463ef3ba69290c63f68969cf
yori <james946@comcast.net>
2025-04-17 21:48:46 -0400

    sb plan skeleton

---
+++ src/sb_plan.py
@@ -0,0 +1,15 @@
+from src.main import Deck
+
+class Matchup:
+    pass
+
+class SideboardPlan:
+    deck: Deck
+    matchups: list[Matchup]
+
+    def __init__(self):
+        pass
+
+    def export():
+        pass
+
