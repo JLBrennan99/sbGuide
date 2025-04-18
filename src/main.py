1dd99181179169981d5e5838e4d3f37e4d636d7c
yori <james946@comcast.net>
2025-04-17 21:34:31 -0400

    added tests

--- src/main.py
+++ src/main.py
@@ -8,8 +8,12 @@ class Deck:
         self.main = main
         self.side = side
 
+    def __eq__(self,other):
+        return self.name == other.name and self.main == other.main and self.side == other.side
+
     @classmethod
     def from_list(cls, name: str, decklist: str):
+        breakpoint()
         mb, sb = decklist.split("\n\n")
         
         main = defaultdict(int)
@@ -38,19 +42,19 @@ class Deck:
     #     this.name = n;  
     # Deck d = new Deck({},{},"titan");
 
-decklist = """1 Swamp
-2 Swamp
+# decklist = """1 Swamp
+# 2 Swamp
 
-1 Mountain
-2 Mountain
-"""
+# 1 Mountain
+# 2 Mountain
+# """
 
 
-deck_1 = Deck("energy", {}, {})
-deck_2 = Deck.from_list("titan", decklist)
+# deck_1 = Deck("energy", {}, {})
+# deck_2 = Deck.from_list("titan", decklist)
 
-print(deck_2.name)
-print(deck_2.main)
-print(deck_2.side)
+# print(deck_2.name)
+# print(deck_2.main)
+# print(deck_2.side)
 
-print(Deck.min_size)
+# print(Deck.min_size)
